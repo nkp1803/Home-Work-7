d714b87562b2ac627e62acb7c9cf3d8146f7f2da
konstantin <nkp.yalta@mail.ru>
2022-07-06 20:10:24 +0300

    exes1

---
+++ exes1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+double[,] GenerateArray(int row, int column, double min, double max)
+{
+    var array = new double[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(min + rnd.NextDouble() * (max - min), 3);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = Prompt("Количество строк >");
+int column = Prompt("Количество столбцов >");
+int min = -10;
+int max = 10;
+double[,] array = GenerateArray(row, column, min, max);
+PrintArray(array);
